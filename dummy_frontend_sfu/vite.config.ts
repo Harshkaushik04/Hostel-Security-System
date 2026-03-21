5205e246072756c96b4419653177e46e071a7dc7
Harsh <harshkaushik4204@gmail.com>
2026-03-21 11:46:09 +0530

    updated it to create ssl certificate for https

--- dummy_frontend_sfu/vite.config.ts
+++ dummy_frontend_sfu/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
+  server: {
+    proxy: {
+      '/ws-api': {
+        target: 'ws://127.0.0.1:2000', // Points to your local Node server
+        ws: true,
+        secure: false
+      }
+    }
+  }
 })
